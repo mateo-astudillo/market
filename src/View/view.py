8aa4d9d36e8b7c540cf9ef695bc096a5994f0aa6
Mateo Astudillo <ast.mateo@gmail.com>
2023-02-28 11:02:42 -0300

    refactor current page

--- src/View/view.py
+++ src/View/view.py
@@ -16,49 +16,19 @@ class View:
 		if self.current_page is not None:
 			self.current_page.hide()
 		pages = {
-			"login": self.login,
-			"register": self.register,
-			"menu": self.menu,
-			"shop": self.shop,
-			"cart": self.cart,
-			"profile": self.profile,
-			"options": self.options,
-			"add": self.add,
-			"edit": self.edit,
+			"login": Login,
+			"register": Register,
+			"menu": Menu,
+			"shop": Shop,
+			"cart": Cart,
+			"profile": Profile,
+			"options": Options,
+			"add": Add,
+			"edit": Edit,
 		}
 		self.current_page = pages.get(page)
-		self.current_page = self.current_page()
+		self.current_page = self.current_page(self)
 		self.current_page.show()
 
 	def logged(self, username):
 		self.controller.logged(username)
-		
-	# Session
-	def login(self):
-		return Login(self)
-
-	def register(self):
-		return Register(self)
-
-	# Customer
-	def menu(self):
-		return Menu(self)
-
-	def shop(self):
-		return Shop(self)
-
-	def cart(self):
-		return Cart(self)
-
-	def profile(self):
-		return Profile(self)
-
-	# Inventory
-	def options(self):
-		return Options(self)
-
-	def add(self):
-		return Add(self)
-
-	def edit(self):
-		return Edit(self)
